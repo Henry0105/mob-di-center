31ad53e0451d3f13e7ee60d0f55db160128984ad
xiaolm <xiaolm@yoozoo.com>
2021-03-03 18:49:59 +0800

    device_base_info 信息，muid as device

---
+++ sbin/mobdi/master/device_base_info/android/android_device_info.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+: '
+@owner: haom
+@describe:
+@projectName:
+@BusinessName:
+@TableRelation:
+'
+
+set -x -e
+
+if [ -z "$1" ]; then
+  exit 1
+fi
+source /home/dba/mobdi_center/conf/hive_db_table.properties
+
+#input
+#dwd_log_device_info_jh_sec_di=dm_mobdi_master.dwd_log_device_info_jh_sec_di
+
+#mapping
+#sysver_mapping_par=dm_sdk_mapping.sysver_mapping_par
+#mapping_carrier_par=dm_mobdi_mapping.mapping_carrier_par
+#brand_model_mapping_par=dm_sdk_mapping.brand_model_mapping_par
+
+#out
+#dws_device_info_di=dm_mobdi_master.dws_device_info_di
+#dws_device_info_full=dm_mobdi_topic.dws_device_info_full
+
+
+insert_day=$1
+prev_1day=`date +%Y%m%d -d "${insert_day} -1 day"`
+
+hive -v -e "
+SET hive.exec.parallel=true;
+SET hive.exec.parallel.thread.number=10;
+set hive.exec.dynamic.partition.mode=nonstrict;
+SET hive.auto.convert.join=true;
+SET hive.map.aggr=true;
+SET hive.merge.mapfiles=true;
+SET hive.optimize.skewjoin = true;
+SET hive.exec.reducers.bytes.per.reducer = 256000000;
+set mapred.max.split.size=256000000;
+set mapred.min.split.size.per.node=128000000;
+set mapred.min.split.size.per.rack=128000000;
+set hive.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
+set hive.merge.smallfiles.avgsize=256000000;
+set hive.merge.size.per.task = 256000000;
+set hive.hadoop.supports.splittable.combineinputformat=true;
+set hive.exec.reducers.max=4000;
+
+with unioned_device_info as (
+  select muid as device, factory, model, screensize, devicetype as devicetype, sysver, cast(breaked as string) as breaked, carrier, serdatetime, 1 as flag
+  FROM $dwd_log_device_info_jh_sec_di
+  where day = '$insert_day'
+  and plat = '1'
+),
+
+ranked_device_info as (
+  select device, factory, model, screensize, devicetype, sysver, breaked, carrier
+  from
+  (
+    select device, factory, model, screensize, devicetype, sysver, breaked, carrier,
+           row_number() over(partition by device order by flag desc, serdatetime desc) as rank
+    FROM unioned_device_info
+    where device is not null
+    and length(device)= 40
+    and device = regexp_extract(device,'([a-f0-9]{40})', 0)
+  ) ranked
+  where rank = 1
+)
+
+insert overwrite table $dws_device_info_di partition(day='$insert_day', plat='1')
+select info.device,
+       info.factory,
+       info.model,
+       info.screensize,
+       info.public_date,
+       info.model_type,
+       info.sysver,
+       info.breaked,
+       info.carrier,
+       info.price,
+       info.devicetype,
+       CASE
+         WHEN lower(trim(info.factory)) in ('null','none','na','other') OR info.factory IS NULL OR trim(info.factory) = '' OR trim(info.factory) = '未知' OR trim(upper(info.factory))='UNKNOWN' THEN 'unknown'
+         ELSE coalesce(upper(trim(info.brand_mapping_brand)), 'other')
+       end AS factory_clean,
+       CASE
+         WHEN lower(trim(info.model)) = 'null' OR info.model IS NULL OR trim(info.model) = '' OR trim(info.model) = '未知' OR upper(trim(info.model)) = 'UNKNOWN' OR upper(trim(info.model))='　　' THEN 'unknown'
+         WHEN brand_mapping_clean_model IS NULL THEN 'other'
+         ELSE trim(upper(info.brand_mapping_clean_model))
+       END AS model_clean,
+       CASE
+         WHEN trim(info.screensize) = '0x0' OR lower(trim(info.screensize)) = 'null' OR info.screensize IS NULL OR trim(info.screensize) = '' OR trim(info.screensize) = '未知' THEN 'unknown'
+         WHEN cast(split(info.screensize, 'x') [0] AS INT) >= cast(split(info.screensize, 'x') [1] AS INT) THEN CONCAT (split(info.screensize, 'x') [1], 'x', split(info.screensize, 'x') [0])
+         WHEN cast(split(info.screensize, 'x') [0] AS INT) < cast(split(info.screensize, 'x') [1] AS INT) THEN CONCAT (split(info.screensize, 'x') [0] ,'x', split(info.screensize, 'x') [1])
+         ELSE 'other'
+       END AS screensize_clean,
+       CASE
+         WHEN info.devicetype is null or trim(info.devicetype) = '' THEN ''
+         ELSE upper(trim(info.devicetype))
+       END AS devicetype_clean,
+       CASE
+         WHEN lower(trim(info.sysver)) = 'null' OR info.sysver IS NULL OR trim(info.sysver) = '' OR trim(info.sysver) = '未知' THEN 'unknown'
+         WHEN info.sysver = sysver_mapping.vernum THEN substr(sysver_mapping.sysver, 1, 3)
+         WHEN substring(regexp_replace(info.sysver, 'Android|[.]|[ ]', ''), 1, 2) in ('11','15','16','20','21','22','23','24','30','31','32','40','41','42','43','44','50','51','60','70','71','80','81','90')
+           THEN CONCAT (substring(regexp_replace(info.sysver, 'Android|[.]|[ ]', ''), 1, 1), '.', substring(regexp_replace(info.sysver, 'Android|[.]|[ ]', ''), 2, 1))
+         ELSE 'unknown'
+       END AS sysver_clean,
+       CASE
+         WHEN lower(trim(info.breaked)) = 'false' THEN false
+         WHEN lower(trim(info.breaked)) = 'true' THEN true
+         ELSE NULL
+       end as breaked_clean,
+       CASE
+         WHEN info.carrier = -1 OR info.carrier ='' or info.carrier is null THEN 'other'
+         when trim(upper(coalesce(carrier_mapping.brand, info.carrier)))=-1 OR trim(upper(coalesce(carrier_mapping.brand, info.carrier)))='' THEN 'unknown'
+         ELSE trim(upper(coalesce(carrier_mapping.brand, info.carrier)))
+       END AS carrier_clean,
+       CASE
+         WHEN lower(trim(info.factory)) in ('null','none','na','other') OR info.factory IS NULL OR trim(info.factory) = '' OR trim(info.factory) = '未知' OR trim(upper(info.factory))='UNKNOWN' THEN 'unknown'
+         ELSE coalesce(upper(trim(info.brand_cn)), 'other')
+       end AS factory_cn,
+       CASE
+         WHEN lower(trim(info.factory)) in ('null','none','na','other') OR info.factory IS NULL OR trim(info.factory) = '' OR trim(info.factory) = '未知' OR trim(upper(info.factory))='UNKNOWN' THEN 'unknown'
+         ELSE coalesce(upper(trim(info.clean_brand_origin)), 'other')
+       end AS factory_clean_subcompany,
+       CASE
+         WHEN lower(trim(info.factory)) in ('null','none','na','other') OR info.factory IS NULL OR trim(info.factory) = '' OR trim(info.factory) = '未知' OR trim(upper(info.factory))='UNKNOWN' THEN 'unknown'
+         ELSE coalesce(upper(trim(info.brand_cn_origin)), 'other')
+       end AS factory_cn_subcompany,
+       coalesce(info.sim_type, '') as sim_type,
+       coalesce(info.screen_size, '') as screen_size,
+       coalesce(info.cpu, '') as cpu
+FROM
+(
+  select ranked_device_info.device,
+         ranked_device_info.factory,
+         ranked_device_info.model,
+         ranked_device_info.screensize,
+         brand_mapping.public_date as public_date,
+         brand_mapping.type as model_type,
+         ranked_device_info.sysver,
+         ranked_device_info.breaked,
+         ranked_device_info.carrier,
+         brand_mapping.price as price,devicetype,
+         brand_mapping.model as brand_mapping_model,
+         brand_mapping.clean_brand as brand_mapping_brand,
+         brand_mapping.clean_model as brand_mapping_clean_model,
+         brand_mapping.brand_cn as brand_cn,
+         brand_mapping.clean_brand_origin as clean_brand_origin,
+         brand_mapping.brand_cn_origin as brand_cn_origin,
+         brand_mapping.sim_type as sim_type,
+         brand_mapping.screen_size as screen_size,
+         brand_mapping.cpu as cpu
+  FROM ranked_device_info
+  LEFT JOIN
+  (
+    select
+    brand, model, public_time_clean as public_date, price, clean_brand, clean_model, brand_cn, clean_brand_origin, brand_cn_origin, sim_type, screen_size, cpu,
+    case
+      when price < 1000 then '低端'
+      when price >= 1000 and price<=2500 then '中端'
+      when price > 2500 then '高端'
+    end as type
+    from $brand_model_mapping_par
+    where version='1000'
+  ) brand_mapping
+  on upper(trim(brand_mapping.brand)) = upper(trim(ranked_device_info.factory))
+  and upper(trim(brand_mapping.model)) = upper(trim(ranked_device_info.model))
+) info
+LEFT JOIN
+(select * from  $sysver_mapping_par  where version='1003')sysver_mapping
+ON info.sysver = sysver_mapping.vernum
+LEFT JOIN
+(select mcc_mnc,brand from $mapping_carrier_par where version='1000') carrier_mapping
+ON info.carrier = carrier_mapping.mcc_mnc
+;
+
+--插入全量表
+insert overwrite table $dws_device_info_full partition(version='${insert_day}.1000', plat='1')
+select device, factory, model, screensize, public_date, model_type, sysver, breaked, carrier, price, devicetype, processtime,model_origin,
+factory_clean, factory_cn, factory_clean_subcompany, factory_cn_subcompany, sim_type, screen_size, cpu
+from
+(
+  select device, factory, model, screensize, public_date, model_type, sysver, breaked, carrier, price, devicetype, processtime,model_origin,
+  factory_clean, factory_cn, factory_clean_subcompany, factory_cn_subcompany, sim_type, screen_size, cpu,
+  row_number() over(partition by device order by processtime desc) as rank
+  from
+  (
+    select device, factory, model_clean as model, screensize_clean as screensize, public_date, model_type, sysver_clean as sysver,
+           breaked_clean as breaked, carrier_clean as carrier, price, devicetype_clean as devicetype, day as processtime,model as model_origin,
+           factory_clean, factory_cn, factory_clean_subcompany, factory_cn_subcompany, sim_type, screen_size, cpu
+    from $dws_device_info_di
+    where day='$insert_day'
+    and plat='1'
+
+    union all
+
+    select device, factory, model, screensize, public_date, model_type, sysver, breaked, carrier, price, devicetype, processtime,model_origin,
+    factory_clean, factory_cn, factory_clean_subcompany, factory_cn_subcompany, sim_type, screen_size, cpu
+    from $dws_device_info_full
+    where version='${prev_1day}.1000'
+    and plat='1'
+  ) unioned
+) ranked
+where rank = 1
+;
+"
---
+++ sbin/mobdi/master/device_base_info/ios/ios_device_info.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+: '
+@owner: xiaolm
+@describe:
+@projectName:
+@BusinessName:
+
+'
+
+set -x -e
+
+if [ -z "$1" ]; then 
+  exit 1
+fi
+source /home/dba/mobdi_center/conf/hive_db_table.properties
+
+insert_day=$1
+prev_1day=`date +%Y%m%d -d "${insert_day} -1 day"`
+
+#input
+#dwd_log_device_info_jh_sec_di=dm_mobdi_master.dwd_log_device_info_jh_sec_di
+
+#mapping
+mapping_ios_factory_par=dm_mobdi_mapping.mapping_ios_factory_par
+mapping_carrier_par=dm_mobdi_mapping.mapping_carrier_par
+
+
+#out
+#dws_device_info_di=dm_mobdi_topic.dws_device_info_di
+#dws_device_info_full=dm_mobdi_topic.dws_device_info_full
+
+
+ios_factory_mapping_sql="
+    add jar hdfs://ShareSdkHadoop/dmgroup/dba/commmon/udf/udf-manager-0.0.7-SNAPSHOT-jar-with-dependencies.jar;
+    create temporary function GET_LAST_PARTITION as 'com.youzu.mob.java.udf.LatestPartition';
+    SELECT GET_LAST_PARTITION('dm_mobdi_mapping', 'mapping_ios_factory_par', 'version');
+"
+ios_factory_mapping_partition=(`hive -e "$ios_factory_mapping_sql"`)
+
+hive -v -e "
+SET hive.exec.parallel=true;
+SET hive.exec.parallel.thread.number=10;
+set hive.exec.dynamic.partition.mode=nonstrict;
+SET hive.auto.convert.join=true; 
+SET hive.map.aggr=true;
+SET hive.merge.mapfiles=true;
+SET hive.optimize.skewjoin = true;
+SET hive.exec.reducers.bytes.per.reducer = 256000000;
+set mapred.max.split.size=256000000;
+set mapred.min.split.size.per.node=128000000;
+set mapred.min.split.size.per.rack=128000000;
+set hive.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
+set hive.merge.smallfiles.avgsize=256000000;
+set hive.merge.size.per.task = 256000000;
+set hive.hadoop.supports.splittable.combineinputformat=true;
+set hive.exec.reducers.max=4000;
+
+with unioned_device_info as (
+  select muid as device, factory, model, screensize, devicetype as devicetype, sysver, cast(breaked as string) as breaked, carrier, serdatetime, 1 as flag
+  FROM $dwd_log_device_info_jh_sec_di
+  where day = '$insert_day'
+  and plat = '2'
+),
+
+ranked_device_info as (
+  select device, factory, model, screensize, devicetype, sysver, breaked, carrier
+  from 
+  (
+    select device, factory, model, screensize, devicetype, sysver, breaked, carrier,
+           row_number() over(partition by device order by flag desc, serdatetime desc) as rank
+    FROM unioned_device_info
+    where device is not null
+    and length(device)= 40
+    and device = regexp_extract(device,'([a-f0-9]{40})', 0)
+  ) ranked
+  where rank = 1
+)
+
+insert overwrite table $dws_device_info_di partition(day='$insert_day', plat='2')
+select info.device,
+       CASE
+         WHEN info.factory IS not NULL and upper(trim(info.factory)) ='APPLE' THEN 'APPLE'
+         WHEN length(info.factory)>0 and upper(trim(info.factory)) !='APPLE' THEN 'APPLE'
+         ELSE ''
+       end AS factory,
+       info.model,
+       info.screensize,
+       info.public_date,
+       info.model_type,
+       info.sysver,
+       info.breaked,
+       info.carrier,  ---需要清洗
+       info.price,
+       info.devicetype,
+       CASE
+         WHEN info.factory IS not NULL and upper(trim(info.factory)) ='APPLE' THEN 'APPLE'
+         ELSE ''
+       end AS factory_clean,
+       CASE
+         WHEN lower(trim(info.model)) = 'null' OR info.model IS NULL OR trim(info.model) = '' OR trim(info.model) = '未知' OR upper(trim(info.model)) = 'UNKNOWN' OR upper(trim(info.model))='　　' THEN 'unknown'
+         WHEN info.model_clean IS NULL THEN 'other'
+         ELSE trim(upper(info.model_clean))
+       END AS model_clean,
+       CASE
+         WHEN trim(info.screensize) = '0x0' OR lower(trim(info.screensize)) = 'null' OR info.screensize IS NULL OR trim(info.screensize) = '' OR trim(info.screensize) = '未知' THEN 'unknown'
+         WHEN cast(split(info.screensize, 'x') [0] AS INT) >= cast(split(info.screensize, 'x') [1] AS INT) THEN CONCAT (split(info.screensize, 'x') [1], 'x', split(info.screensize, 'x') [0])
+         WHEN cast(split(info.screensize, 'x') [0] AS INT) < cast(split(info.screensize, 'x') [1] AS INT) THEN CONCAT (split(info.screensize, 'x') [0], 'x', split(info.screensize, 'x') [1])
+         ELSE 'other'
+       END AS screensize_clean,
+       CASE
+         WHEN info.devicetype is null or trim(info.devicetype) = '' THEN ''
+         ELSE upper(trim(info.devicetype))
+       END AS devicetype_clean,
+       CASE
+         WHEN lower(trim(info.sysver)) != 'null' and info.sysver IS not NULL and info.sysver rlike '^[a-zA-Z0-9,. ]*$' THEN info.sysver
+         ELSE ''
+       END AS sysver_clean,
+       CASE
+         WHEN lower(trim(info.breaked)) = 'false' THEN false
+         WHEN lower(trim(info.breaked)) = 'true' THEN true
+         ELSE NULL
+       end as breaked_clean,
+       CASE
+         WHEN info.carrier = -1 OR info.carrier ='' OR info.carrier IS NULL THEN 'other'
+         when trim(upper(coalesce(carrier_mapping.brand, info.carrier)))=-1 OR trim(upper(coalesce(carrier_mapping.brand, info.carrier)))='' THEN 'unknown'
+         ELSE trim(upper(coalesce(carrier_mapping.brand, info.carrier)))
+       END AS carrier_clean,
+       '','','','','',''
+from
+(
+  select ranked_device_info.device,
+         ranked_device_info.factory,
+         ranked_device_info.model,
+         ranked_device_info.screensize,
+         iosm.public_date,
+         '' model_type,
+         ranked_device_info.sysver,
+         ranked_device_info.breaked,
+         ranked_device_info.carrier,
+         case when iosm.price ='' or iosm.price is null then 'unknown' else iosm.price end as price,
+         devicetype,
+		 iosm.model as model_clean
+  from ranked_device_info
+  left join
+  (
+    select *
+    from $mapping_ios_factory_par
+    where version = '$ios_factory_mapping_partition'
+  ) iosm on upper(iosm.model_code) = upper(ranked_device_info.model)
+) info
+LEFT JOIN
+(select mcc_mnc,brand from $mapping_carrier_par where version='1000') carrier_mapping
+ON info.carrier = carrier_mapping.mcc_mnc
+;
+
+--插入全量表
+insert overwrite table $dws_device_info_full partition(version='${insert_day}.1000', plat='2')
+select device, factory, model, screensize, public_date, model_type, sysver, breaked, carrier, price, devicetype, processtime,model_origin,
+'','','','','','',''
+from 
+(
+  select device, factory, model, screensize, public_date, model_type, sysver, breaked, carrier, price, devicetype, processtime,model_origin,
+         row_number() over(partition by device order by processtime desc) as rank
+  from 
+  (
+    select device, factory, model_clean as model, screensize_clean as screensize, public_date, model_type, sysver_clean as sysver,
+           breaked_clean as breaked, carrier_clean as carrier, price, devicetype_clean as devicetype, day as processtime, model as model_origin
+    from $dws_device_info_di
+    where day ='$insert_day'
+    and plat='2'
+
+    union all
+
+    select device, factory, model, screensize, public_date, model_type, sysver, breaked, carrier, price, devicetype, processtime ,model_origin
+    from $dws_device_info_full
+    where version = '${prev_1day}.1000'
+    and plat='2'
+  ) unioned
+) ranked
+where rank = 1
+;
+"
