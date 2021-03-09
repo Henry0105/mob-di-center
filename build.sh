1b4dfa5326da8aceacac497973a758234cc4655d
xiaolm <xiaolm@yoozoo.com>
2021-03-09 18:51:30 +0800

    build 修改

--- build.sh
+++ build.sh
@@ -18,31 +18,23 @@ project_root=/home/dba
 # mvn clean scalastyle:check package -P$1
 mvn clean package -P$1
 
-latest=$(readlink -f ${mobdi_HOME}/)
-version=$(cat ${mobdi_HOME}/dist/conf/version.properties)
-home_version_control=$project_root/version_control/${version}
+MOBDI_HOME=/data/walle/mobdi_center
+version=$(cat ${MOBDI_HOME}/dist/conf/version.properties)
+mkdir -p /home/dba/mobdi_center_versions
 
-email_report=$project_root/mobdi_center/email_report
-kpi_qc=$project_root/mobdi_center/kpi_qc
-mobdi=$project_root/mobdi_center/mobdi
-mobdi_sec=$project_root/mobdi_center/mobdi_sec
-sort_system=$project_root/mobdi_center/sort_system
-crawler=$project_root/mobdi_center/crawler
-conf=$project_root/mobdi_center/conf
+ln -snf ${MOBDI_HOME} /home/dba/mobdi_center_versions/${version}
 
-ln -snf ${mobdi_HOME}  ${home_version_control}
-#ln -snf ${home_version_control}/dist/sbin/email_report  ${email_report}
-#ln -snf ${home_version_control}/dist/sbin/kpi_qc  ${kpi_qc}
-ln -snf ${home_version_control}/dist/sbin/mobdi  ${mobdi}
-#ln -snf ${home_version_control}/dist/sbin/mobdi_sec  ${mobdi_sec}
-#ln -snf ${home_version_control}/dist/sbin/sort_system  ${sort_system}
-#ln -snf ${home_version_control}/dist/sbin/crawler  ${crawler}
-ln -snf ${home_version_control}/dist/sbin/conf  ${conf}
+ln -snf /home/dba/mobdi_center_versions/${version}/dist/sbin/mobdi /home/dba/mobdi_center
 
-yes|cp -fr ${home_version_control}/dist/lib $project_root/
+yes|cp -fr /home/dba/mobdi_center_versions/${version}/dist/lib /home/dba/mobdi_center/lib
 
 # update_info=`git diff --stat  head~ head`
 
 # java -jar ./lib/mailSender-1.0.0.jar "walle" "sucess" "zhoup@mob.com,zhtli@mob.com"
 # 将所有脚本加上可执行权限
 chmod +x `find -iname \*.sh `
+
+
+
+
+
