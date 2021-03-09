e04842b1669722d0f0cbac26234b383f7aae2038
luost <luost@yoozoo.com>
2021-03-09 20:16:24 +0800

    dws层脚本修改

---
+++ sbin/mobdi/tag/location_tag/frequency/android/device_position_classify.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+set -x -e
+
+: '
+@owner:zhoup
+@describe:计算常去地是城市还是乡村
+@projectName:MobDI
+'
+
+#入参
+daypre=$1
+day=${daypre:0:6}01
+
+#导入配置文件
+source /home/dba/mobdi_center/conf/hive_db_tb_sdk_mapping.properties
+source /home/dba/mobdi_center/conf/hive_db_tb_report.properties
+
+#input
+#rp_device_frequency_3monthly=dm_mobdi_report.rp_device_frequency_3monthly
+
+#mapping
+#chinese_area_code_new=dm_sdk_mapping.chinese_area_code_new
+
+#output
+#ads_device_frequencry_position_classify=dm_mobdi_report.ads_device_frequencry_position_classify
+
+hive -e"
+set hive.optimize.index.filter=true;
+set hive.exec.orc.zerocopy=true;
+set hive.vectorized.execution.enabled=true;
+set hive.optimize.ppd=true;
+
+set hive.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
+SET hive.merge.mapfiles=true;
+SET hive.merge.mapredfiles=true;
+set mapred.max.split.size=256000000;
+set mapred.min.split.size.per.node=250000000;
+set mapred.min.split.size.per.rack=250000000;
+set hive.merge.smallfiles.avgsize=250000000;
+set hive.merge.size.per.task=250000000;
+set hive.exec.reducers.bytes.per.reducer=256000000;
+
+with frequency_info as(
+  select device,
+         cnt,
+         case 
+           when substring(area,length(area),1) in ('区','市','岛') then 1 
+           when substring(area,length(area),1) in ('县','旗') then 2
+         else 0 end as flag
+  from
+  (
+    select frequency.device,
+           frequency.cnt,
+           area_mapping.area
+    from
+    (
+      select device,
+             area,
+             cnt
+      from $rp_device_frequency_3monthly
+      where day='$day'
+      and length(trim(area))>0
+    ) frequency
+    inner join $chinese_area_code_new area_mapping
+    on frequency.area = area_mapping.area_code
+  )t
+)
+
+insert overwrite table $ads_device_frequencry_position_classify partition(day =$day)
+select device,
+       case
+         when sum(unben_sum) >sum(rural_sum) then 1
+         when sum(unben_sum) <sum(rural_sum) then 2
+         else 3
+       end as type
+from
+(
+  select 
+    device,
+    case when flag=1 then sum(cnt) else 0 end as  unben_sum,
+    case when flag=2 then sum(cnt) else 0 end as  rural_sum
+  from frequency_info
+  where flag <> 0
+  group by device, flag
+)tt
+group by device;
+"
---
+++ sbin/mobdi/tag/online_tag/device_install_cnt.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+set -x -e
+
+:<<!
+计算app的在装
+!
+
+if [ $# -ne 1 ]; then
+    echo "ERROR: wrong number of parameters"
+    echo "USAGE: <day>"
+    exit 1
+fi
+
+#入参
+day=$1
+
+#导入配置文件
+source /home/dba/mobdi_center/conf/hive_db_tb_topic.properties
+source /home/dba/mobdi_center/conf/hive_db_tb_sdk_mapping.properties
+source /home/dba/mobdi_center/conf/hive_db_tb_report.properties
+
+#input
+#rp_device_profile_full_view=dm_mobdi_report.rp_device_profile_full_view
+
+#mapping
+#p2p_app_cat=dm_sdk_mapping.p2p_app_cat
+
+#output
+#ads_device_install_app_cash_p2p_cnt_di=dm_mobdi_report.ads_device_install_app_cash_p2p_cnt_di
+#ads_device_install_app_cash_cnt_view=dm_mobdi_report.ads_device_install_app_cash_cnt_view
+#ads_device_install_app_p2p_cnt_view=dm_mobdi_report.ads_device_install_app_p2p_cnt_view
+#timewindow_online_profile_v3=dm_mobdi_report.timewindow_online_profile_v3
+
+hive -v -e"
+SET hive.exec.dynamic.partition=true;  
+SET hive.exec.dynamic.partition.mode=nonstrict;
+SET hive.exec.max.dynamic.partitions=1000;
+SET hive.exec.max.dynamic.partitions.pernode=1000;
+set hive.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
+SET hive.merge.mapfiles=true;
+SET hive.merge.mapredfiles=true;
+set mapred.max.split.size=250000000;
+set mapred.min.split.size.per.node=128000000;
+set mapred.min.split.size.per.rack=128000000;
+set hive.merge.smallfiles.avgsize=250000000;
+set hive.merge.size.per.task = 250000000;
+
+insert overwrite table $ads_device_install_app_cash_p2p_cnt_di partition(type,day)
+select device,
+       concat_ws(',',collect_set(app_mapping.pkg)) as applist,
+       count(app_mapping.pkg) as cnt,
+       app_mapping.cate_id as type,
+       '$day' as day
+from 
+(
+  select device,app
+  from $rp_device_profile_full_view
+  lateral view explode(split(applist,',')) apps as app
+) profile_full
+inner join
+(
+  select pkg ,cate_id
+  from $p2p_app_cat
+  where cat in ('P2P借贷','现金贷')
+)app_mapping
+on profile_full.app=app_mapping.pkg
+group by device,app_mapping.cate_id;
+
+create or replace view $ads_device_install_app_p2p_cnt_view as
+select device,applist,cnt,type,day as processtime
+from $ads_device_install_app_cash_p2p_cnt_di
+where type=1
+and day=$day;
+
+create or replace view $ads_device_install_app_cash_cnt_view as
+select device,applist,cnt,type,day as processtime
+from $ads_device_install_app_cash_p2p_cnt_di
+where type=5
+and day=$day;
+"
+
+
+#添加标签转换到v3表
+hive -v -e "
+SET mapreduce.map.memory.mb=8192;
+set mapreduce.map.java.opts='-Xmx8192M';
+set mapreduce.child.map.java.opts='-Xmx8192M';
+set mapreduce.reduce.memory.mb=8192;
+set mapreduce.reduce.java.opts=-Xmx8192m;
+set mapred.reduce.tasks=4000;
+SET hive.merge.mapfiles=true;
+SET hive.merge.mapredfiles=true;
+set mapred.max.split.size=250000000;
+set mapred.min.split.size.per.node=128000000;
+set mapred.min.split.size.per.rack=128000000;
+set hive.merge.smallfiles.avgsize=250000000;
+set hive.merge.size.per.task = 250000000;
+set hive.exec.dynamic.partition=true;
+set hive.exec.dynamic.partition.mode=nonstrict;
+
+insert overwrite table $timewindow_online_profile_v3 partition(day=${day},feature)
+select device,cnt,'5353_1000' as feature
+from dm_mobdi_master.device_install_app_count
+where day=${day}
+and type=5
+
+union all
+
+select device,cnt,'5354_1000' as feature
+from dm_mobdi_master.device_install_app_count
+where day=${day}
+and type=1;
+"
