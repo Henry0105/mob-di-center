e118f5e966ebc3db731fb61247de85c2ef51a666
yanhw <yanhw@yoozoo.com>
2021-03-09 17:33:57 +0800

    外部conf目录的properties文件打包进来,改回原来的读取配置文件方式

--- actions-spark2/src/main/scala/com/youzu/mob/utils/PropUtils.scala
+++ actions-spark2/src/main/scala/com/youzu/mob/utils/PropUtils.scala
@@ -2,10 +2,6 @@ package com.youzu.mob.utils
 
 import com.typesafe.config.ConfigFactory
 
-import java.io.{File, FileInputStream, InputStreamReader}
-import java.util.Properties
-import scala.util.{Failure, Success, Try}
-
 object PropUtils {
 
   private val propList = Array("application.properties", "hive_db_tb_master.properties",
@@ -13,36 +9,8 @@ object PropUtils {
     "hive_db_tb_sdk_mapping.properties", "hive_db_tb_topic.properties"
   )
 
-//  lazy private val prop = propList.map(ConfigFactory.load).reduce((a, b) => a.withFallback(b))
+  lazy private val prop = propList.map(ConfigFactory.load).reduce((a, b) => a.withFallback(b))
 
-  def getProperty(key: String): String = prop.getProperty(key)
+  def getProperty(key: String): String = prop.getString(key)
 
-  // 需要外部 赋值 MOBDI_HOME
-  lazy val prop = {
-    val prop = new Properties()
-    Try {
-      propList
-        .foreach(propFile => {
-          val configPath = s"${System.getenv("MOBDI_HOME")}/conf/${propFile}"
-          val file = new File(configPath)
-          val propIn = if (file.exists()) {
-            //  yarn-client模式加载外部资源文件
-            new InputStreamReader(new FileInputStream(file))
-          } else if (new File(propFile).exists()) {
-            // yarn-cluster模式加载外部资源文件
-            new InputStreamReader(new FileInputStream(new File(propFile)))
-          } else {
-            // 本地idea测试
-            new InputStreamReader(new FileInputStream(new File(
-              "./" + propFile)))
-          }
-          prop.load(propIn)
-          propIn.close()
-        })
-    } match {
-      case Success(_) =>
-      case Failure(e) =>
-    }
-    prop
-  }
 }
