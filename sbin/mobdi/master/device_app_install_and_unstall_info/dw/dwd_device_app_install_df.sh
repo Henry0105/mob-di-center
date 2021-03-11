8211958bd303dc4db73f13293127776a897523ef
luost <luost@yoozoo.com>
2021-03-11 16:21:50 +0800

    dws层脚本修改

--- sbin/mobdi/master/device_app_install_and_unstall_info/dw/dwd_device_app_install_df.sh
+++ sbin/mobdi/master/device_app_install_and_unstall_info/dw/dwd_device_app_install_df.sh
@@ -14,4 +14,4 @@ spark2-submit --master yarn \
 --conf spark.sql.shuffle.partitions=100 \
 --conf spark.executor.memoryOverhead=2048 \
 --conf spark.driver.maxResultSize=5g \
-/home/dba/lib/MobDI-center-spark2-1.0-SNAPSHOT.jar $day "appInstallDF"
+/home/dba/mobdi_center/lib/MobDI-center-spark2-1.0-SNAPSHOT.jar $day "appInstallDF"
