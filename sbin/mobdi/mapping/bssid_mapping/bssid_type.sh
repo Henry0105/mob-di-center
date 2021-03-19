ca258fd19938f4d80717856547e8a8d6b5d1d7ad
wujinshou <wujinshou@mob.com>
2021-03-19 17:38:42 +0800

    dim_bssid_type_all_mf muid 更新 source

--- sbin/mobdi/mapping/bssid_mapping/bssid_type.sh
+++ sbin/mobdi/mapping/bssid_mapping/bssid_type.sh
@@ -10,7 +10,9 @@ set -e -x
 
 day=$1
 pday=`date -d "$day -31 days" "+%Y%m%d"`
-
+#导入配置文件
+source /home/dba/mobdi_center/conf/hive_db_tb_mobdi_mapping.properties
+source /home/dba/mobdi_center/conf/hive_db_tb_master.properties
 #input
 #dwd_location_info_sec_di=dm_mobdi_master.dwd_location_info_sec_di
 #tmp
