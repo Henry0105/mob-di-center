315be6aa89b402e6b3c8d7c62d9e0bf974d4a401
wujinshou <wujinshou@mob.com>
2021-03-29 10:52:30 +0800

    脚本提变量修改

---
+++ sbin/mobdi/tag/device_whitelist/device_unstall_install/device_install_pre.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+set -e -x
+
+#导入配置文件
+source /home/dba/mobdi_center/conf/hive_db_tb_mobdi_mapping.properties
+
+# input
+unstall_install_risk_pre=${dm_mobdi_tmp}.unstall_install_risk_pre
+# output
+device_install_1month=${dm_mobdi_tmp}.device_install_1month
+device_install_2month=${dm_mobdi_tmp}.device_install_2month
+device_install_3month=${dm_mobdi_tmp}.device_install_3month
+
+day=$1
+
+p1months=`date -d "$day -30 days" +%Y%m%d`
+p2months=`date -d "$day -60 days" +%Y%m%d`
+p3months=`date -d "$day -90 days" +%Y%m%d`
+
+hive -e"
+insert overwrite table $device_install_1month
+  select device, cnt_pkg, cnt_all 
+  from 
+  (
+    select device, count(*) over(partition by device) as cnt_pkg, sum(cnt) over(partition by device) as cnt_all
+    from 
+    (
+      select device, pkg, count(*) as cnt
+    from $unstall_install_risk_pre
+    where day between '$p1months' and '$day' and refine_final_flag = 1
+      group by device, pkg
+    ) as b 
+  ) as c 
+  group by device, cnt_pkg, cnt_all
+"
+
+
+hive -e"
+insert overwrite table $device_install_2month
+  select device, cnt_pkg, cnt_all 
+  from 
+  (
+    select device, count(*) over(partition by device) as cnt_pkg, sum(cnt) over(partition by device) as cnt_all 
+    from 
+    (
+      select device, pkg, count(*) as cnt
+      from $unstall_install_risk_pre
+      where day between '$p2months' and '$day' and refine_final_flag = 1
+      group by device, pkg
+    ) as b 
+  ) as c 
+  group by device, cnt_pkg, cnt_all
+"
+
+hive -e"
+insert overwrite table $device_install_3month
+  select device, cnt_pkg, cnt_all 
+  from 
+  (
+    select device, count(*) over(partition by device) as cnt_pkg, sum(cnt) over(partition by device) as cnt_all 
+    from 
+    (
+      select device, pkg, count(*) as cnt
+      from $unstall_install_risk_pre
+      where day between '$p3months' and '$day' and refine_final_flag = 1
+      group by device, pkg
+    ) as b 
+  ) as c 
+  group by device, cnt_pkg, cnt_all
+"
+
