fe872f88b8ee2b13b043987246217a679e85a124
wujinshou <wujinshou@mob.com>
2021-03-29 18:40:51 +0800

    脚本标量按规范更新

---
+++ sbin/mobdi/tag/device_whitelist/simulator_device.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e -x
+
+day=$1
+
+p1month=`date -d "$day -30 days" +%Y%m%d`
+
+source /home/dba/mobdi_center/conf/hive_db_tb_master.properties
+
+# input
+#dwd_simulator_det_info_sec_di=dm_mobdi_master.dwd_simulator_det_info_sec_di
+# output
+device_simulator_full=${dm_mobdi_tmp}.device_simulator_full
+
+hive -e"
+insert overwrite table $device_simulator_full
+select device ,model
+from(
+  select device ,model from $device_simulator_full
+  union all
+  select muid as device, model
+  from $dwd_simulator_det_info_sec_di
+  where day >= '$p1month' and day <= '$day' and qemukernel = '1'
+)t
+group by device,model
+"
