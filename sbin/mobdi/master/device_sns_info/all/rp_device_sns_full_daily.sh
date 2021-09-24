4723694bd5c8fe175b30b2f54209632869fc19a5
wujinshou <qwert12345>
2021-09-24 12:17:50 +0800

    更新社交信息表代码

--- sbin/mobdi/master/device_sns_info/all/rp_device_sns_full_daily.sh
+++ sbin/mobdi/master/device_sns_info/all/rp_device_sns_full_daily.sh
@@ -8,7 +8,6 @@
 @businessname: sns
 @sourcetable: dw_mobdi_etl.dwd_log_share_new_di,dw_mobdi_etl.dwd_log_oauth_new_di,dw_mobdi_md.sns_tmp_oauth,dw_mobdi_md.sns_tmp_share,rp_mobdi_app.rp_device_sns_full
 @targettable: dw_mobdi_md.sns_tmp_share,dw_mobdi_md.sns_tmp_oauth,rp_mobdi_app.rp_device_sns_full
-
 @TableRelation:dw_mobdi_etl.dwd_log_share_new_di->dw_mobdi_md.sns_tmp_share|dw_mobdi_etl.dwd_log_oauth_new_di->dw_mobdi_md.sns_tmp_oauth|dw_mobdi_md.sns_tmp_share,dw_mobdi_md.sns_tmp_oauth->rp_mobdi_app.rp_device_sns_full
 '
 
@@ -94,7 +93,46 @@ from(
            educationjson,snsuserlevel,snsregat,favouritecount,gender,secret,followercount,workjson,processtime,
     row_number() over (partition by device,plat,snsplat,snsuid order by processtime desc)  as rank
     from(
-        select  deviceid as device,
+        select  muid as device,
+                plat,
+                snsplat,
+                snsuid,
+                '' as nickname,
+                '' icon,
+                case
+                when split(snsuserpartdata,'\\\\|')[0]='0' then '0'
+                when split(snsuserpartdata,'\\\\|')[0]='1' then '1'
+                when split(snsuserpartdata,'\\\\|')[0]='2' then '-1'
+                else ''
+                end as gender,
+                '' as  snsuserurl,
+                '' as  resume,
+                '' as secret,
+                split(snsuserpartdata,'\\\\|')[2] as secrettype,
+                case
+                when length(from_unixtime(floor(split(snsuserpartdata,'\\\\|')[1]/1000),'yyyyMMdd')) = 8
+                and DATEDIFF(CURRENT_DATE,from_unixtime(floor(split(snsuserpartdata,'\\\\|')[1]/1000),'yyyy-MM-dd')) < 100*365
+                and DATEDIFF(CURRENT_DATE,from_unixtime(floor(split(snsuserpartdata,'\\\\|')[1]/1000),'yyyy-MM-dd')) > 5*365
+                then from_unixtime(floor(split(snsuserpartdata,'\\\\|')[1]/1000),'yyyyMMdd')
+                else ''
+                end as birthday,
+                '' as followercount,
+                '' as favouritecount,
+                '' as sharecount,
+                '' as snsregat,
+                '' as snsuserlevel,
+                split(snsuserpartdata,'\\\\|')[3] as educationjson,
+                split(snsuserpartdata,'\\\\|')[4] as workjson,
+                day as processtime
+        from    $dwd_log_share_new_di
+        where   day = ${indate}
+                and length(trim(muid)) = 40
+                and length(trim(snsuserpartdata)) > 4
+                and plat=1
+
+                union all
+
+                select  deviceid as device,
                 plat,
                 snsplat,
                 snsuid,
@@ -129,6 +167,7 @@ from(
         where   day = ${indate}
                 and length(trim(deviceid)) = 40
                 and length(trim(snsuserpartdata)) > 4
+                and plat=2
         )a1
     )aa
 where rank = 1;
@@ -189,6 +228,55 @@ from
     row_number() over (partition by device,plat,snsplat,snsuid order by processtime desc) as rank
     from
         (
+        select muid as device,
+            plat,
+            snsplat,
+            snsuid,
+            case
+            when length(from_unixtime(floor(snsuserdata['birthday']/1000),'yyyyMMdd')) = 8
+            and DATEDIFF(CURRENT_DATE,from_unixtime(floor(snsuserdata['birthday']/1000),'yyyy-MM-dd')) < 100*365
+            and DATEDIFF(CURRENT_DATE,from_unixtime(floor(snsuserdata['birthday']/1000),'yyyy-MM-dd')) > 5*365
+            then from_unixtime(floor(snsuserdata['birthday']/1000),'yyyyMMdd')
+            else ''
+            end as birthday,
+            snsuserdata['icon'] as icon,
+            case
+            when trim(snsuserdata['secretType'])='-1' then '-1'
+            when trim(snsuserdata['secretType'])='0' then '0'
+            when trim(snsuserdata['secretType'])='1' then '1'
+            else ''
+            end as secrettype,
+            snsuserdata['nickname'] as nickname,
+            snsuserdata['snsUserUrl'] as snsuserurl,
+            snsuserdata['shareCount'] as sharecount,
+            snsuserdata['resume'] as resume,
+            snsuserdata['educationJSON'] as educationjson,
+            snsuserdata['snsUserLevel'] as snsuserlevel,
+            case
+            when length(from_unixtime(floor(snsuserdata['snsregat']/1000),'yyyyMMdd')) = 8 then from_unixtime(floor(snsuserdata['snsregat']/1000),'yyyyMMdd')
+            else ''
+            end as snsregat,
+            snsuserdata['favouriteCount'] as favouritecount,
+            case
+            when trim(snsuserdata['gender'])='0' then '0'
+            when trim(snsuserdata['gender'])='1' then '1'
+            when trim(snsuserdata['gender'])='2' then '-1'
+            else ''
+            end as gender,
+            snsuserdata['secret'] as secret,
+            snsuserdata['followerCount'] as followercount,
+            snsuserdata['workJSON'] as workjson,
+            day as processtime,
+            rawuserdata
+        from $dwd_log_oauth_new_di
+        where day = ${indate}
+              and length(trim(muid)) = 40
+              and size(snsuserdata)>1
+              and (snsuserdata['birthday'] <> '' or snsuserdata['icon'] <> '' or snsuserdata['secretType'] <> '' or snsuserdata['nickname'] <> '' or snsuserdata['snsUserUrl'] <> '' or snsuserdata['shareCount'] <> '' or snsuserdata['resume'] <> '' or snsuserdata['educationJSON'] <> '' or snsuserdata['snsUserLevel'] <> '' or snsuserdata['snsregat'] <> '' or snsuserdata['favouriteCount'] <> '' or snsuserdata['gender'] <> '' or snsuserdata['secret'] <> '' or snsuserdata['followerCount'] <> '' or snsuserdata['workJSON'] <> '')
+              and plat=1
+
+              union all
+
         select deviceid as device,
             plat,
             snsplat,
@@ -234,6 +322,7 @@ from
               and length(trim(deviceid)) = 40
               and size(snsuserdata)>1
               and (snsuserdata['birthday'] <> '' or snsuserdata['icon'] <> '' or snsuserdata['secretType'] <> '' or snsuserdata['nickname'] <> '' or snsuserdata['snsUserUrl'] <> '' or snsuserdata['shareCount'] <> '' or snsuserdata['resume'] <> '' or snsuserdata['educationJSON'] <> '' or snsuserdata['snsUserLevel'] <> '' or snsuserdata['snsregat'] <> '' or snsuserdata['favouriteCount'] <> '' or snsuserdata['gender'] <> '' or snsuserdata['secret'] <> '' or snsuserdata['followerCount'] <> '' or snsuserdata['workJSON'] <> '')
+              and plat=2
         ) a
     ) aa
 where rank = 1;
