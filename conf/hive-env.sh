2b89807ac9e86a743279a23c8e39d32d040ecadc
yanhw <yanhw@yoozoo.com>
2021-11-03 11:49:27 +0800

    环境变量调整,使用绝对路径

--- conf/hive-env.sh
+++ conf/hive-env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-cd `dirname $0`
+conf_path=/home/dba/mobdi_center/conf
 echo "加载hive表变量..."
-source ./hive_db_tb_dashboard.properties
-source ./hive_db_tb_master.properties
-source ./hive_db_tb_mobdi_mapping.properties
-source ./hive_db_tb_report.properties
-source ./hive_db_tb_sdk_mapping.properties
-source ./hive_db_tb_topic.properties
-source ./hive_db_tb_tmp.properties
+source $conf_path/hive_db_tb_dashboard.properties
+source $conf_path/hive_db_tb_master.properties
+source $conf_path/hive_db_tb_mobdi_mapping.properties
+source $conf_path/hive_db_tb_report.properties
+source $conf_path/hive_db_tb_sdk_mapping.properties
+source $conf_path/hive_db_tb_topic.properties
 echo "hive表变量加载完毕"
