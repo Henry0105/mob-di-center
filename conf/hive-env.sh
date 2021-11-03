fda755267c1c4094ef013b5d29b2d6b900a8c055
yanhw <yanhw@yoozoo.com>
2021-11-03 17:37:38 +0800

    添加配置文件

--- conf/hive-env.sh
+++ conf/hive-env.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 conf_path=/home/dba/mobdi_center/conf
 echo "加载hive表变量..."
+source $conf_path/hive_db.properties
 source $conf_path/hive_db_tb_dashboard.properties
 source $conf_path/hive_db_tb_master.properties
 source $conf_path/hive_db_tb_mobdi_mapping.properties
+source $conf_path/hive_db_tb_ods.properties
+source $conf_path/hive_db_tb_other.properties
 source $conf_path/hive_db_tb_report.properties
 source $conf_path/hive_db_tb_sdk_mapping.properties
 source $conf_path/hive_db_tb_topic.properties
+source $conf_path/hive_db_tb_tp_mobdi_model.properties
+
 echo "hive表变量加载完毕"
