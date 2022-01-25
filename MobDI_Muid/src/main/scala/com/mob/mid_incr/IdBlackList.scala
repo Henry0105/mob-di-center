6f13beffd354deaf81dc520858dd85feb415fdf0
luost <luost@yoozoo.com>
2022-01-25 15:59:04 +0800

    mid增量逻辑

--- MobDI_Muid/src/main/scala/com/mob/mid_incr/IdBlackList.scala
+++ MobDI_Muid/src/main/scala/com/mob/mid_incr/IdBlackList.scala
@@ -15,6 +15,7 @@ object IdBlackList {
       .enableHiveSupport()
       .getOrCreate()
 
+    //更新ieid_oiid_asid关系数据
     spark.sql(
       s"""
          |INSERT OVERWRITE TABLE dm_mid_master.ieid_oiid_asid_base PARTITION(day = '$day')
@@ -42,6 +43,35 @@ object IdBlackList {
          |GROUP BY ieid,oiid,asid
          |""".stripMargin)
 
+    //更新duid_ieid_oiid关系
+    spark.sql(
+      s"""
+         |INSERT OVERWRITE TABLE dm_mid_master.duid_ieid_oiid_base PARTITION(day = '$day')
+         |SELECT duid
+         |     , ieid
+         |     , oiid
+         |FROM
+         |(
+         |  SELECT duid
+         |       , ieid
+         |       , oiid
+         |  FROM dm_mid_master.duid_incr_tmp
+         |  WHERE (ieid <> '' OR oiid <> '')
+         |  AND duid <> ''
+         |  AND day = '$day'
+         |  GROUP BY ieid,oiid,duid
+         |
+         |  UNION ALL
+         |
+         |  SELECT duid
+         |       , ieid
+         |       , oiid
+         |  FROM dm_mid_master.duid_ieid_oiid_base
+         |  WHERE day = '$pday'
+         |)a
+         |GROUP BY duid,ieid,oiid
+         |""".stripMargin)
+
     List("ieid", "oiid", "asid").par.foreach(id => {
       spark.sql(
         s"""
@@ -58,8 +88,13 @@ object IdBlackList {
            |  WHERE TRIM($id) <> ''
            |  AND day = '$day'
            |) a
-           |LEFT ANTI JOIN dm_mid_master.${id}_blacklist_full b
-           |ON a.${id} = b.${id}
+           |LEFT ANTI JOIN
+           |(
+           |  SELECT $id
+           |  FROM dm_mid_master.${id}_blacklist_full
+           |  WHERE day < '$day'
+           |)b
+           |ON a.$id = b.$id
            |""".stripMargin).createOrReplaceTempView(s"${id}_tmp")
 
       id match {
@@ -79,7 +114,6 @@ object IdBlackList {
 
     spark.sql(
       s"""
-         |INSERT OVERWRITE TABLE dm_mid_master.${id(0)}_blacklist_full PARTITION(day='$day')
          |SELECT ${id(0)}
          |FROM
          |(
@@ -90,6 +124,60 @@ object IdBlackList {
          |  GROUP BY ${id(0)}
          |)a
          |WHERE (SIZE(${id(1)}_list) > $count1 OR SIZE(${id(2)}_list) > $count2)
-         |""".stripMargin)
+         |""".stripMargin).createOrReplaceTempView(s"${id(0)}_blacklist_$day")
+
+    if (id(0) == "asid") {
+      spark.sql(
+        s"""
+           |INSERT OVERWRITE TABLE dm_mid_master.${id(0)}_blacklist_full PARTITION(day='$day')
+           |SELECT ${id(0)}
+           |FROM ${id(0)}_blacklist_$day
+           |""".stripMargin)
+    } else {
+      spark.sql(
+        s"""
+           |INSERT OVERWRITE TABLE dm_mid_master.${id(0)}_blacklist_full PARTITION(day='$day')
+           |SELECT ${id(0)}
+           |FROM
+           |(
+           |  SELECT ${id(0)}
+           |  FROM
+           |  (
+           |    SELECT ${id(0)}
+           |         , COLLECT_SET(duid) AS duid_list
+           |    FROM
+           |    (
+           |      SELECT /*+ BROADCAST(b) */
+           |             duid
+           |           , ${id(0)}
+           |      FROM
+           |      (
+           |        SELECT duid
+           |             , ${id(0)}
+           |        FROM dm_mid_master.duid_ieid_oiid_base
+           |        WHERE day = '$day'
+           |      )a
+           |      LEFT ANTI JOIN
+           |      (
+           |        SELECT $id
+           |        FROM dm_mid_master.${id}_blacklist_full
+           |        WHERE day < '$day'
+           |      )b
+           |      ON a.${id(0)} = b.${id(0)}
+           |    )c
+           |    GROUP BY ${id(0)}
+           |  )d
+           |  WHERE SIZE(duid_list) > 200
+           |
+           |  UNION ALL
+           |
+           |  SELECT ${id(0)}
+           |  FROM ${id(0)}_blacklist_$day
+           |)e
+           |GROUP BY ${id(0)}
+           |""".stripMargin)
+    }
+
+
   }
 }
