ad9148ab28c0e0cead8f18c70a13ed4733e93489
铜镜 <871765337@qq.com>
2022-03-18 15:48:53 +0800

    mid相关代码更新

--- MobDI_Muid/src/main/resources/v2/gen_flag.sh
+++ MobDI_Muid/src/main/resources/v2/gen_flag.sh
@@ -22,18 +22,32 @@ SET hive.exec.parallel=true;
 set hive.mapjoin.smalltable.filesize=500000000;
 
 create temporary table mobdi_test.black_ieid as
-select ieid,count(distinct mid) cnt,1 ieid_flag from $id_source where day='all' and coalesce(ieid,'')<>''
+select ieid,count(distinct mid) cnt,1 ieid_flag
+from $id_source where day='all' and coalesce(ieid,'')<>''
 group by ieid having count(distinct mid) > 1;
 
 create temporary table mobdi_test.black_oiid as
-select oiid,factory,count(distinct mid) cnt,2 oiid_flag from $id_source where day='all' and coalesce(oiid,'')<>''
+select oiid,factory,count(distinct mid) cnt,2 oiid_flag
+from $id_source where day='all' and coalesce(oiid,'')<>''
 group by oiid,factory having count(distinct mid) > 1;
 
+create temporary table mobdi_test.black_mid_ieid as
+select mid,count(distinct ieid) cnt,4 mid_ieid_flag
+from $id_source where day='all' and coalesce(ieid,'')<>''
+group by mid having count(distinct ieid) > 3;
+
+create temporary table mobdi_test.black_mid_oiid as
+select mid,count(distinct oiid) cnt,8 mid_oiid_flag
+from $id_source where day='all' and coalesce(oiid,'')<>''
+group by mid having count(distinct oiid) > 2;
+
 insert overwrite table $id_source partition(day='all')
-select duid,oiid,ieid,factory,model,unid,mid,
-coalesce(ieid_flag,0) + coalesce(oiid_flag,0) as  flag
+select a.duid,a.oiid,a.ieid,a.factory,model,unid,a.mid,
+coalesce(ieid_flag,0) + coalesce(oiid_flag,0) + coalesce(mid_ieid_flag,0) + coalesce(mid_oiid_flag,0) as  flag
 from $id_source a
 left join mobdi_test.black_ieid b on a.ieid=b.ieid
-left join mobdi_test.black_oiid c on a.oiid=c.oiid on a.factory=c.factory
+left join mobdi_test.black_oiid c on a.oiid=c.oiid and a.factory=c.factory
+left join mobdi_test.black_mid_ieid e on a.mid=e.mid
+left join mobdi_test.black_mid_oiid f on a.mid=f.mid
 where day='all'
 "
